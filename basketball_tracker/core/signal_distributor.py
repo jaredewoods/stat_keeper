aedfcde66b7506cac532294abfa00bb620dacefb
jaredewoods <155901160+jaredewoods@users.noreply.github.com>
2024-07-14 17:39:40 -0500

    Add new signal functions to SignalDistributor
    This commit expands the functionalities of the SignalDistributor class by adding various signal functions related to actions like clicking buttons for "back to zero", "back 20", "back 10," changing playback speed, capturing, pausing,

--- basketball_tracker/core/signal_distributor.py
+++ basketball_tracker/core/signal_distributor.py
@@ -6,9 +6,18 @@ from PyQt6.QtCore import QObject, pyqtSignal
 class SignalDistributor(QObject):
 
     SIG_DebugMessage = pyqtSignal(str)
-    SIG_CaptureButtonClicked = pyqtSignal()
     SIG_RosterPlayerSelected = pyqtSignal(str)
     SIG_EventCodeSelected = pyqtSignal(str)
+
+    SIG_BackToZeroButtonClicked = pyqtSignal()
+    SIG_Back20ButtonClicked = pyqtSignal()
+    SIG_Back10ButtonClicked = pyqtSignal()
+    SIG_ChangePlaybackSpeedButtonClicked = pyqtSignal()
+    SIG_CaptureButtonClicked = pyqtSignal()
+    SIG_CapturePauseButtonClicked = pyqtSignal()
+    SIG_PauseButtonClicked = pyqtSignal()
     SIG_UndoButtonClicked = pyqtSignal()
+    SIG_PlayButtonClicked = pyqtSignal()
+    SIG_LogEntriesButtonClicked = pyqtSignal()
 
     print("1 SignalDistributor Initialized")
